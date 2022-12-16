e2ebe3651a98968f9d69e4c2c5d9cb105af4cfe0
Chris <hello@chris-saez.com>
2022-12-16 03:38:39 -0800

    add function to avoid sequential and double decimals in numbers

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function calculate() { // calculate the equation displayed on the screen
+    if (lastOperator() == "+" ||
+                lastOperator() == "-" ||
+                lastOperator() == "÷" ||
+                lastOperator() == "%" ||
+                lastOperator() == "*") {
+        userInput = userInput.substring(0, userInput.length-3); 
+    }
     const calculation = eval(userInput);
     userInput = calculation;
     screenInput.innerHTML = calculation;
@@ -9,27 +16,46 @@ function lastOperator() { // returns the last operator of the string sequence di
     return lastOperator;
 }
 
-function clear() {
+function clear() { // clears all the numbers from screen and start from nothing
     userInput = '';
     screenInput.innerHTML = userInput;
 }
 
+function lastSection(input) { // splits the last number section of the screen where the user is inputting numbers
+    let inputSplit = input.split('');
+    let lastSpaceIndex = inputSplit.findLastIndex((space) => space == ' ');
+    let lastSection = inputSplit.slice(lastSpaceIndex+1);
+    return lastSection;
+}
+
+function duplicateDecimal(input) { // removes last decimal of the string sequence, removes it, and adds the decimal to the end of the string
+    let inputSplit = input.split('');
+    let lastSpaceIndex = inputSplit.findLastIndex((space) => space == ' ');
+    let lastSection = inputSplit.slice(lastSpaceIndex+1);
+    let endHalf = lastSection.slice(lastSection.indexOf('.')+1);
+    let startHalf = inputSplit.slice(0, inputSplit.lastIndexOf('.'));
+    let updatedSplit = startHalf.concat(endHalf, '.');
+    return updatedSplit.join('');
+}
+
 let userInput = '';
 
-const numberButtons = document.querySelectorAll("#number, .decimal");
+const numberButtons = document.querySelectorAll("#number");
 const opButtons = document.querySelectorAll("#operators");
 const screenInput = document.querySelector("#input");
 const equalsButton = document.querySelector("#equals-sign");
 const clearButton = document.querySelector(".clear");
+const decimalButton = document.querySelector(".decimal");
 
-// if (screenInput.innerHTML == '') {
-//     screenInput.innerHTML = 'Enter calculations here';
-//     screenInput.style.fontSize = '16px';
-//     screenInput.style.opacity = '30%';
-// } else {
-//     screenInput.style.fontSize = '24px';
-//     screenInput.style.opacity = '100%';
-// }
+decimalButton.addEventListener("click", ()=> {
+    const decimalInput = decimalButton.innerHTML;
+    if(lastSection(userInput).includes('.')){
+        userInput = duplicateDecimal(userInput);
+    } else {
+        userInput+=decimalInput;
+    }
+    screenInput.innerHTML=userInput;
+})
 
 for (const numberButton of numberButtons) { // adds number to the end of string when button is clicked
     numberButton.addEventListener("click", ()=>{
